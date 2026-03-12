02eb071ba94aec43a26825c748e5574072bf5404
Frederic Stark <frederic.stark@gmail.com>
2026-03-12 11:39:23 +0100

    Step 4.3: Add WireBus class for runtime inter-device signal routing

---
+++ src/core/wire_bus.cpp
@@ -0,0 +1,45 @@
+/*
+	wire_bus.cpp
+
+	Runtime inter-device signal routing implementation.
+
+	Part of Phase 4: Device Interface & Machine Object.
+*/
+
+#include "core/wire_bus.h"
+
+void WireBus::init(int numWires)
+{
+	numWires_ = numWires;
+	// Initialize all wires to 1 (matching AddrSpac_Init behavior)
+	wires_.fill(1);
+	for (auto& cbs : changeCallbacks_) cbs.clear();
+	for (auto& cbs : pulseCallbacks_)  cbs.clear();
+}
+
+void WireBus::set(int wireId, uint8_t val)
+{
+	if (wires_[wireId] != val) {
+		wires_[wireId] = val;
+		for (auto& cb : changeCallbacks_[wireId]) {
+			cb();
+		}
+	}
+}
+
+void WireBus::onChange(int wireId, ChangeCallback cb)
+{
+	changeCallbacks_[wireId].push_back(std::move(cb));
+}
+
+void WireBus::onPulse(int wireId, ChangeCallback cb)
+{
+	pulseCallbacks_[wireId].push_back(std::move(cb));
+}
+
+void WireBus::pulse(int wireId)
+{
+	for (auto& cb : pulseCallbacks_[wireId]) {
+		cb();
+	}
+}
